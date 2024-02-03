e3605dce2cef365025501d84af120faf8b15bd12
Brandon Fontes <brandonjfontes@gmail.com>
2024-02-02 17:32:04 -0800

    made the primary button take you to my website

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-let name = 'brandon';
+let person = {
+    name: 'brandon',
+    age: 21
+};
 
-console.log(name);
+console.log(person);
+
+document.getElementById("myButton").onclick = function () {
+    location.href = "https://www.amazon.com/?tag=amazusnavi-20&hvadid=675149237887&hvpos=&hvnetw=g&hvrand=13612276634782283294&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9028844&hvtargid=kwd-10573980&ref=pd_sl_7j18redljs_e&hydadcr=28883_14649097";
+};
