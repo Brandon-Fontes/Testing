374e1831c4014a0172e8850a58684f5cbebcd8a0
Brandon Fontes <brandonjfontes@gmail.com>
2024-02-03 16:30:08 -0800

    added new navbar and played with api calling

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let person = {
-    name: 'brandon',
-    age: 21
-};
-
-console.log(person);
-
-document.getElementById("myButton").onclick = function () {
-    location.href = "https://www.amazon.com/?tag=amazusnavi-20&hvadid=675149237887&hvpos=&hvnetw=g&hvrand=13612276634782283294&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9028844&hvtargid=kwd-10573980&ref=pd_sl_7j18redljs_e&hydadcr=28883_14649097";
-};
+fetch('https:////api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0', {
+    method: 'POST',
+    headers: {
+        'content-Type': 'application/json'
+    },
+    body: JSon.stringify({
+        name: 'User 1'
+    })
+}).then(res =>{
+        return res.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
