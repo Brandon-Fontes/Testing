2745b3d1c3c886411c1a0149a2677ff070b70b4b
Brandon Fontes <brandonjfontes@gmail.com>
2024-02-11 12:26:32 -0800

    ditching this for now

--- db.js
+++ db.js
@@ -1,13 +1,4 @@
-const express =  require("express")
-const { config } = require('./config.js')
-const userName = require('./index.js')
-
-
-const {
-    createPool
-} = require('mysql');
-
-const pool = createPool({
+var pool = createPool({
     host: "localhost",
     user: "root",
     password: config,
@@ -15,12 +6,16 @@ const pool = createPool({
     connectionLimit: 10
 })
 
-pool.query('select * from first_table', (err, result, fields) =>{
+var result = pool.query('select * from first_table', (err, result, fields) =>{
     if(err){
-        return console.log(err);
+        console.log(err);
+        return err;
     }
-    return console.log(result);
+    console.log(result);
+    return result;
 })
 
 
-
+module.exports = {
+    pool
+};
--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const config = require('./config.js');
+import 'express';
+//const { Pool } = require('undici-types');
+import { config } from './config.js';
+import { createPool } from 'mysql';
+import inquirer from 'inquirer';
+import './db.js';
 
-function promptMe(){
+
+
+
+ 
+function promptMe() {
     var userAdjective = prompt("What is your username");
     alert (userAdjective);
     console.log(userAdjective);
+    pool.query('insert into first_table (user_id) values ('+ userAdjective +')', (err, result, fields) =>{
+        if(err){
+            return console.log(err);
+        }
+        return console.log(result);
+    })
 }
+
+
+
